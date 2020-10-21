f4ba1579e43530a804fa786e3c9dea3b3b256995
vostrikovaviktoria <vostrikovaviktoria@yandex.ru>
2020-10-21 11:39:05 +0300

    Change fields to registration
    Signed-off-by: vostrikovaviktoria <vostrikovaviktoria@yandex.ru>

--- web_service/messenger/registration/models.py
+++ web_service/messenger/registration/models.py
@@ -3,21 +3,13 @@ from django.conf import settings
 
 class Information(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    color = models.CharField(max_length=20, verbose_name="Color", default='1')
-    age = models.CharField(max_length=2, verbose_name="Age", default='1')
-    gender = models.CharField(max_length=2, verbose_name="Gender", default='1')
-    season = models.CharField(max_length=2, verbose_name="Season", default='1')
-    sсope = models.CharField(max_length=2, verbose_name="Scope", default='1')
-    purpose = models.CharField(max_length=2, verbose_name="Purpose", default='1')
-    count_fr = models.CharField(max_length=2, verbose_name="Count_friends", default='1')
-    choice1 = models.CharField(max_length=2, verbose_name="Choice1", default='1')
-    choice2 = models.CharField(max_length=2, verbose_name="Choice2", default='1')
-    choice3 = models.CharField(max_length=2, verbose_name="Choice3", default='1')
-    choice4 = models.CharField(max_length=2, verbose_name="Choice4", default='1')
-    choice5 = models.CharField(max_length=2, verbose_name="Choice5", default='1')
-    theme = models.CharField(max_length=2, verbose_name="Theme", default='1')
-    pref = models.CharField(max_length=2, verbose_name="Preference", default='1')
-    pref2 = models.CharField(max_length=100, verbose_name="Preference_menu", default='1')
+    color = models.CharField(max_length=20, verbose_name="Color", default=1)
+    age = models.IntegerField(verbose_name="Age", default=1)
+    gender = models.IntegerField(verbose_name="Gender", default=1)
+    season = models.IntegerField(verbose_name="Season", default=1)
+    sсope = models.IntegerField(verbose_name="Scope", default=1)
+    purpose = models.IntegerField(verbose_name="Purpose", default=1)
+    count_fr = models.IntegerField(verbose_name="Count_friends", default=1)
  
     def __str__(self):
         return 'Профиль пользователя {}'.format(self.user.username)
--- web_service/messenger/registration/views.py
+++ web_service/messenger/registration/views.py
@@ -14,24 +14,16 @@ def registration(request):
             inf.user = new_user
             inf.age = inf_form.cleaned_data['age']
             inf.gender = inf_form.cleaned_data['gender']
-            inf.color = inf_form.cleaned_data['color']
+
+            color = ""
+            for i in inf_form.cleaned_data['color']:
+                color += i
+
+            inf.color = color
             inf.season = inf_form.cleaned_data['season']
             inf.scope = inf_form.cleaned_data['scope']
             inf.purpose = inf_form.cleaned_data['purpose']
             inf.count_fr = inf_form.cleaned_data['count_fr']
-            inf.choice1 = inf_form.cleaned_data['choice1']
-            inf.choice2 = inf_form.cleaned_data['choice2']
-            inf.choice3 = inf_form.cleaned_data['choice3']
-            inf.choice4 = inf_form.cleaned_data['choice4']
-            inf.choice5 = inf_form.cleaned_data['choice5']
-            inf.theme = inf_form.cleaned_data['theme']
-            inf.pref = inf_form.cleaned_data['pref']
-
-            pref = ""
-            for i in inf_form.cleaned_data['pref2']:
-                pref += i
-
-            inf.pref2 = pref
             inf.save()
             return render(request, 'registration/registration_success.html')
     else:
