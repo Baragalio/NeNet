7380ca92090e175108077b0d6eefb1e1932ebb7c
Baragalio <72518906+Baragalio@users.noreply.github.com>
2020-10-23 16:45:20 +0300

    neural network with a forecast Converting to a vector burn function updated1 prediction - model.predict

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ print ('Begin')
 print(1)
 print("Vostrikova add")
 print("Teselkin add")
+
+model = load_model('model.h5')
+def prediction(registration_data):
+	output = model.predict(registration_data)
+	return output
